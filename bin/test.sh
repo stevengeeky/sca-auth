a9542861333cfe33006306eeb031db33427b962c
Soichi Hayashi <hayashis@soichi7.ppa.iu.edu>
2015-12-17 23:13:58 +0000

    updated resetpass desc

--- bin/test.sh
+++ bin/test.sh
@@ -1,8 +1,8 @@
 #issue token
-./auth.js issue --scopes '{ "common": ["user"] }' --sub 'test_service' #--out test.jwt
+#./auth.js issue --scopes '{ "common": ["user"] }' --sub 'test_service' #--out test.jwt
 
 #./auth.js modscope --username hayashis --set '{"common": ["user", "admin"]}'
-#./auth.js modscope --username hayashis --add '{"websh": ["open_session"]}'
+./auth.js modscope --username hayashis --add '{"common": ["admin"]}'
 #./auth.js modscope --username hayashis --del '{"websh": ["deleteme"]}'
 
 #./auth.js modscope --username sundar --add '{"dicom": ["admin"]}'
