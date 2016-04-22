367076a10cfef3b21987d53031b29f170e747d56
Soichi Hayashi <hayashis@iu.edu>
2016-04-22 01:21:00 +0000

    handle null fullname

--- ui/js/app.js
+++ ui/js/app.js
@@ -324,7 +324,7 @@ app.filter('propsFilter', function() {
         for (var i = 0; i < keys.length; i++) {
           var prop = keys[i];
           var text = props[prop].toLowerCase();
-          if (item[prop].toString().toLowerCase().indexOf(text) !== -1) {
+          if (item[prop] && item[prop].toString().toLowerCase().indexOf(text) !== -1) {
             itemMatches = true;
             break;
           }
