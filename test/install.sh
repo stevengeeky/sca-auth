b8909df3aea8b924aa1cac2e311f7a2c814c1179
Soichi Hayashi <soichih@gmail.com>
2016-11-18 16:26:30 +0000

    debugging test install.sh

--- test/install.sh
+++ test/install.sh
@@ -11,6 +11,12 @@ if [ ! -f api/config/auth.key ]; then
     )
 fi
 
+echo "point 1"
+if [ ! -f api/config/test.jwt ]; then
+    echo "creating test.jwt"
+    ./bin/auth.js issue --scopes '{ "sca": ["user"] }' --sub 'test_service' --out test.jwt
+fi
+
 if [ ! -f api/config/index.js ]; then
     echo "installing test config/index.js"
     cp api/config/index.js.sample api/config/index.js 
@@ -20,10 +26,4 @@ if [ ! -f api/config/ldap.password ]; then
     echo "testpass" > api/config/ldap.password
 fi
 
-if [ ! -f api/config/test.jwt ]; then
-    echo "creating test.jwt"
-    (
-    cd bin
-    ./auth.js issue --scopes '{ "sca": ["user"] }' --sub 'test_service' --out test.jwt
-    )
-fi
+
