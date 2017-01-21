e1d76b355a4bedebdfa2e2e15e3fce4d3e976463
Soichi Hayashi <soichih@gmail.com>
2017-01-21 15:10:41 +0000

    docker build step update

--- docker/build.sh
+++ docker/build.sh
@@ -1,2 +1,3 @@
-docker build .. -t soichih/sca-auth:1.0.0 -t soichih/sca-auth:latest
-
+docker build -t soichih/sca-auth ..
+docker tag soichih/sca-auth soichih/sca-auth:1.0.0
+docker push soichih/sca-auth
--- docker/push.sh
+++
@@ -1,3 +0,0 @@
-docker push soichih/sca-auth:1.0.0 
-docker push soichih/sca-auth:latest
-
