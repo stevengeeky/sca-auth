a0f6b003d4e02ad4dc15dfd4d0a139e505f1f54c
Michael Johnson <mj82@globalnoc.iu.edu>
2019-06-17 21:13:02 +0000

    Make sendmail default mailer; change wording #3

--- api/common.js
+++ api/common.js
@@ -57,7 +57,7 @@ function do_send_email_confirmation(url, user, cb) {
         from: config.local.email_confirmation.from,
         to: user.email,
         subject: config.local.email_confirmation.subject,
-        text: "Hello!\n\nIf you have created a new SCA account, please visit following URL to confirm your email address.\n\n"+ fullurl,
+        text: "Hello!\n\nIf you have created a new pSConfig Web Admin (PWA)  account, please visit following URL to confirm your email address.\n\n"+ fullurl,
         //html:  ejs.render(html_template, params),
     }, function(err, info) {
         if(err) return cb(err);
