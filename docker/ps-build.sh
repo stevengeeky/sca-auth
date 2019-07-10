6e52db12849b0e1ac10db18597556212cd458c53
Michael Johnson <mj82@grnoc.iu.edu>
2019-07-10 14:59:53 +0000

    Update build script

--- docker/ps-build.sh
+++ docker/ps-build.sh
@@ -3,5 +3,5 @@ if [ ! $? -eq 0 ]; then
     echo "failed to build"
     exit
 fi
-docker tag perfsonar/sca-auth perfsonar/sca-auth:1.0.2
+docker tag perfsonar/sca-auth perfsonar/sca-auth
 docker push perfsonar/sca-auth
